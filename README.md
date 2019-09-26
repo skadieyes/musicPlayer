9f4d0265a88c46a253041e06e0a0e595721617df
zhengranqiu <zhengranqiu@maoyan.com>
2019-09-26 18:05:06 +0800

    readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # 一个音乐播放器
+## getJsonData
+    1. 使用puppeteer模拟打开一个chorme, 并访问页面
+    2. 通过puppeteer的launch方法生成了一个browser的实例，对应于浏览器，launch方法可以传入配置项，比较有用的是在本地调试时传入{headless:false}可以关闭headless模式。
+    3. 监听并拦截页面的ajax请求
+    4. 根据收到的数据用fs模块的appendFile写入到list.json中，这样就拿到了音乐的地址，也是数据的源头
+## downloadMusic
+    1. 拿到音乐，批量把音乐下载到本地
+## uploadMusicQiNiu
+    1. 到七牛云去注册，获得你的ACCESS_KEY和SECRET_KEY，通过实名认证之后获得一个免费的空间
+    2. 读取下载到本地文件，遍历上传到七牛云空间上
+## app
+    1. 现在来做一个静态播放器，本地的list.json就是歌曲列表
+    2. 使用h5 audio来播放音乐，如果需要自定义audio的样式，可以隐藏audio的ui，使用自定义的ui绑定audio元素的事件来控制audio。
--- system/getJsonData.js
+++ system/getJsonData.js
@@ -1,6 +1,4 @@
-/*
-* https://zhaoqize.github.io/puppeteer-api-zh_CN/#/
-* */
+
 const puppeteer = require('puppeteer-core')
 const fs = require('fs')
 
@@ -11,30 +9,22 @@ let jsonDataList = [];
         headless: true
     })
     const page = await browser.newPage()
-    await page.goto('http://music.wandhi.com/?name=%E8%A1%80%E8%85%A5%E7%88%B1%E6%83%85%E6%95%85%E4%BA%8B&type=netease')
+    await page.goto('http://music.wandhi.com/?name=No%20roots&type=netease')
 
     // 根据网站规则，监听页面ajax请求，截获请求数据
     page.on('response', async res => {
         const url = res.url()
-        console.log('url')
-        console.log(url)
         if (url === 'http://music.wandhi.com/') {
             const data = await res.json()
             const list = data.data
-            console.log('list')
-            console.log(list)
             jsonDataList.push(...list)
 
             // 词曲分离分别写入各自文件
             function writeData() {
                 let list = jsonDataList.map(item => {
                     const lrcJson = JSON.stringify({lrc: item.lrc}, null, 4)
-                    console.log('写入songid.json')
-                    console.log(item)
-                    if (item.songid !== 31311140) return
+                    if (item.songid !== 444324644) return
                     fs.appendFile(`./system/musicLrc/${item.songid}.json`, lrcJson, (e, result) => {
-                        console.log(item.songid)
-                        console.log(result)
                         if (e) {
                             console.error(e)
                         }
